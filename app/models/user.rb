b8400bf747ed815d3aad845ec6c72aa0889bb8b9
Daniel Kehoe <daniel@danielkehoe.com>
2014-08-27 18:29:15 -0700

    rails_apps_composer: add roles to a User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
