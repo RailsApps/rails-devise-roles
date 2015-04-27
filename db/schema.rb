f6696728ec8f277f40155c78eca170d637b03339
Daniel Kehoe <daniel@danielkehoe.com>
2015-04-27 22:15:05 +0800

    before_filter becomes before_action

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150101171737) do
+ActiveRecord::Schema.define(version: 20140828012915) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
